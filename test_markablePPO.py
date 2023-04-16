cd494bce78b2236e93c045763c4a2ea41b320067
jacob975 <z123a123s123@gmail.com>
2023-04-17 07:38:08 +0800

    Minor fix

--- test_markablePPO.py
+++ test_markablePPO.py
@@ -11,7 +11,7 @@ from custom_policy import CustomCNN
 import torch as th
 
 n_env = 4
-batch_size = 1024
+batch_size = 4096
 n_epochs = 5
 
 
@@ -32,24 +32,24 @@ model = MaskablePPO(
     tensorboard_log="./markableppo_chess_tensorboard/"
 )
 # Reload the model
-model = MaskablePPO.load(
-    "best_model", env=env, verbose=1,
-    n_epochs=n_epochs,
-    batch_size=batch_size,
-    policy_kwargs=policy_kwargs,
-    tensorboard_log="./markableppo_chess_tensorboard/"
-)
+#model = MaskablePPO.load(
+#    "best_model", env=env, verbose=1,
+#    n_epochs=n_epochs,
+#    batch_size=batch_size,
+#    policy_kwargs=policy_kwargs,
+#    tensorboard_log="./markableppo_chess_tensorboard/"
+#)
 
 adversary = MaskablePPO(
     "MlpPolicy", env, gamma=0.4, seed=None, verbose=1,
     policy_kwargs=policy_kwargs,
 )
-callback = SetAdversaryCallback(update_freq=2048*n_env, adversary=adversary)
+callback = SetAdversaryCallback(update_freq=1024*n_env, adversary=adversary)
 model.learn(
     1e7, callback=callback,
-    tb_log_name="resnet18-batch1024-env4-epoch5-ppo", 
+    tb_log_name="resnet18-batch4096-env4-epoch5-ppo", 
     reset_num_timesteps=False # Important to keep the same number of timesteps
 )
 
-model.save("resnet18-batch1024-env4-epoch5-ppo")
+model.save("resnet18-batch4096-env4-epoch5-ppo")
 del model # remove to demonstrate saving and loading
